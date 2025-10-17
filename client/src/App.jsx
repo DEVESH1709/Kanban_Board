e4724e432f719ca72c9f88ee645152c1488495c7
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-10-17 13:05:14 +0530

    feat: initialize state for task management in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-
+import {useState} from 'react'
 
 function App() {
-  
+
+  const [task,setTasks] = useState([]);
+ 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Devesh
-      </h1>
-    </>
+   <div className="app-root min-h-screen bg-white">
+   <header className="app-header px-6 py-4 border-b border-gray-200">
+<div className="flex items-center gap-4">
+<h1 className="text-2xl font-bold">Kanban Board (Styling)</h1>
+</div>
+   </header>
+
+   <div>
+
+   </div>
+
+   </div>
   )
 }
 
